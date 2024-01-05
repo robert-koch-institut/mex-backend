0417b3db7292e07bd3a59e7e8d68bdcd98592d8f
Carolin Scholl <schollc@rki.de>
2024-01-05 17:52:13 +0100

    directly use merged model classes from mex-common

--- mex/backend/merged/transform.py
+++ mex/backend/merged/transform.py
@@ -6,6 +6,7 @@ from mex.backend.graph.models import GraphResult
 from mex.backend.graph.transform import transform_search_result_to_model
 from mex.backend.merged.models import MergedItemSearchResponse
 from mex.common.logging import logger
+from mex.common.models import MERGED_MODEL_CLASSES_BY_NAME
 
 
 def transform_graph_results_to_merged_item_search_response_facade(
@@ -28,8 +29,12 @@ def transform_graph_results_to_merged_item_search_response_facade(
         try:
             model = transform_search_result_to_model(result)
             model_dict = model.model_dump()
+            # create a MergedModel class with the dictionary
             model_dict["entityType"] = f"Merged{model_dict['entityType']}"
-            items.append(model_dict)
+            del model_dict["hadPrimarySource"]
+            del model_dict["identifierInPrimarySource"]
+            model_class = MERGED_MODEL_CLASSES_BY_NAME[model_dict["entityType"]]
+            items.append(model_class.model_validate(model_dict))
         except Neo4jError as error:  # pragma: no cover
             logger.exception(
                 "%s\n__node__\n  %s\n__refs__\n%s\n",
