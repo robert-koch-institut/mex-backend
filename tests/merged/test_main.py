d748137612e9eed0b5c8cffec81c0a5f4a88e441
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2024-09-05 15:18:36 +0200

    adapt test for merged endpoint

--- tests/merged/test_main.py
+++ tests/merged/test_main.py
@@ -25,31 +25,40 @@ def test_search_merged_items_mocked(
         {
             "items": [
                 {
-                    "identifier": unit.stableTargetId,
-                    "identifierInPrimarySource": unit.identifierInPrimarySource,
-                    "stableTargetId": unit.stableTargetId,
-                    "email": ["test@foo.bar"],
-                    "entityType": "ExtractedOrganizationalUnit",
-                    "_refs": [
+                    "components": [
                         {
-                            "label": "hadPrimarySource",
-                            "position": 0,
-                            "value": "2222222222222222",
-                        },
-                        {
-                            "label": "name",
-                            "position": 0,
-                            "value": {
-                                "value": "Eine unit von einer Org.",
-                                "language": "de",
-                            },
-                        },
-                        {
-                            "label": "name",
-                            "position": 1,
-                            "value": {"value": "A unit of an org.", "language": "en"},
-                        },
+                            "identifier": unit.identifier,
+                            "identifierInPrimarySource": unit.identifierInPrimarySource,
+                            "stableTargetId": unit.stableTargetId,
+                            "email": ["test@foo.bar"],
+                            "entityType": "ExtractedOrganizationalUnit",
+                            "_refs": [
+                                {
+                                    "label": "hadPrimarySource",
+                                    "position": 0,
+                                    "value": "2222222222222222",
+                                },
+                                {
+                                    "label": "name",
+                                    "position": 0,
+                                    "value": {
+                                        "value": "Eine unit von einer Org.",
+                                        "language": "de",
+                                    },
+                                },
+                                {
+                                    "label": "name",
+                                    "position": 1,
+                                    "value": {
+                                        "value": "A unit of an org.",
+                                        "language": "en",
+                                    },
+                                },
+                            ],
+                        }
                     ],
+                    "entityType": "MergedOrganizationalUnit",
+                    "identifier": unit.stableTargetId,
                 }
             ],
             "total": 14,
@@ -172,6 +181,34 @@ def test_search_merged_items_mocked(
                 "total": 1,
             },
         ),
+        (
+            "?identifier=bFQoRhcVH5DHUB",
+            {
+                "items": [
+                    {
+                        "$type": "MergedOrganizationalUnit",
+                        "alternativeName": [],
+                        "email": [],
+                        "identifier": "bFQoRhcVH5DHUB",
+                        "name": [
+                            {"language": "en", "value": "Unit 1.6"},
+                            {"language": "en", "value": "Unit 1.7"},
+                        ],
+                        "parentUnit": "bFQoRhcVH5DHUv",
+                        "shortName": [],
+                        "unitOf": [],
+                        "website": [
+                            {
+                                "language": None,
+                                "title": "Unit Homepage",
+                                "url": "https://unit-1-7",
+                            }
+                        ],
+                    }
+                ],
+                "total": 1,
+            },
+        ),
         ("?identifier=thisIdDoesNotExist", {"items": [], "total": 0}),
     ],
     ids=[
@@ -180,10 +217,11 @@ def test_search_merged_items_mocked(
         "entity type contact points",
         "full text search",
         "identifier filter",
+        "identifier filter with composite result",
         "empty result",
     ],
 )
-@pytest.mark.usefixtures("load_dummy_data")
+@pytest.mark.usefixtures("load_dummy_data", "load_dummy_rule_set")
 @pytest.mark.integration
 def test_search_merged_items(
     client_with_api_key_read_permission: TestClient,
