d3208335baa4c8e4e9c38825c8e9941dbb06f464
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2024-09-10 13:54:19 +0200

    add test cases for three rules and no rules

--- tests/merged/test_helpers.py
+++ tests/merged/test_helpers.py
@@ -23,6 +23,7 @@ from mex.common.models import (
     SubtractivePerson,
 )
 from mex.common.types import Identifier, Text, TextLanguage
+from tests.conftest import MockedGraph
 
 
 def test_merge_extracted_items_and_apply_preventive_rule() -> None:
@@ -268,3 +269,200 @@ def test_search_merged_items_in_graph() -> None:
         ],
         "total": 1,
     }
+
+
+@pytest.mark.parametrize(
+    ("mocked_graph_result", "expected"),
+    [
+        (
+            [
+                {
+                    "items": [
+                        {
+                            "components": [
+                                {
+                                    "identifier": "jbZ5Br9Vninm08ptYZFxW",
+                                    "identifierInPrimarySource": "unit-1",
+                                    "stableTargetId": "e5rfAc2p5zV39WUVZeAR1",
+                                    "email": ["test@foo.bar"],
+                                    "entityType": "ExtractedOrganizationalUnit",
+                                    "_refs": [
+                                        {
+                                            "label": "hadPrimarySource",
+                                            "position": 0,
+                                            "value": "2222222222222222",
+                                        },
+                                        {
+                                            "label": "name",
+                                            "position": 0,
+                                            "value": {
+                                                "value": "Eine unit von einer Org.",
+                                                "language": "de",
+                                            },
+                                        },
+                                    ],
+                                }
+                            ],
+                            "entityType": "MergedOrganizationalUnit",
+                            "identifier": "e5rfAc2p5zV39WUVZeAR1",
+                        }
+                    ],
+                    "total": 1,
+                }
+            ],
+            {
+                "items": [
+                    {
+                        "email": ["test@foo.bar"],
+                        "identifier": "e5rfAc2p5zV39WUVZeAR1",
+                        "name": [
+                            {"language": "de", "value": "Eine unit von einer Org."},
+                        ],
+                    }
+                ],
+                "total": 1,
+            },
+        ),
+        (
+            [
+                {
+                    "items": [
+                        {
+                            "components": [
+                                {
+                                    "identifier": "jbZ5Br9Vninm08ptYZFxW",
+                                    "identifierInPrimarySource": "unit-1",
+                                    "stableTargetId": "e5rfAc2p5zV39WUVZeAR1",
+                                    "email": ["test@foo.bar"],
+                                    "entityType": "ExtractedOrganizationalUnit",
+                                    "_refs": [
+                                        {
+                                            "label": "hadPrimarySource",
+                                            "position": 0,
+                                            "value": "2222222222222222",
+                                        },
+                                        {
+                                            "label": "name",
+                                            "position": 0,
+                                            "value": {
+                                                "value": "Eine unit von einer Org.",
+                                                "language": "de",
+                                            },
+                                        },
+                                    ],
+                                },
+                                {
+                                    "stableTargetId": "e5rfAc2p5zV39WUVZeAR1",
+                                    "email": ["bar@foo.bar"],
+                                    "entityType": "AdditiveOrganizationalUnit",
+                                    "_refs": [],
+                                },
+                            ],
+                            "entityType": "MergedOrganizationalUnit",
+                            "identifier": "e5rfAc2p5zV39WUVZeAR1",
+                        }
+                    ],
+                    "total": 1,
+                }
+            ],
+            "Unexpected number of rules found in graph: 1",
+        ),
+        (
+            [
+                {
+                    "items": [
+                        {
+                            "components": [
+                                {
+                                    "identifier": "jbZ5Br9Vninm08ptYZFxW",
+                                    "identifierInPrimarySource": "unit-1",
+                                    "email": ["test@foo.bar"],
+                                    "entityType": "ExtractedOrganizationalUnit",
+                                    "_refs": [
+                                        {
+                                            "label": "hadPrimarySource",
+                                            "position": 0,
+                                            "value": "2222222222222222",
+                                        },
+                                        {
+                                            "label": "name",
+                                            "position": 0,
+                                            "value": {
+                                                "value": "Eine unit von einer Org.",
+                                                "language": "de",
+                                            },
+                                        },
+                                        {
+                                            "label": "stableTargetId",
+                                            "position": 0,
+                                            "value": "e5rfAc2p5zV39WUVZeAR1",
+                                        },
+                                    ],
+                                },
+                                {
+                                    "email": ["bar@foo.bar"],
+                                    "entityType": "AdditiveOrganizationalUnit",
+                                    "_refs": [
+                                        {
+                                            "label": "stableTargetId",
+                                            "position": 0,
+                                            "value": "e5rfAc2p5zV39WUVZeAR1",
+                                        },
+                                    ],
+                                },
+                                {
+                                    "email": ["test@foo.bar"],
+                                    "entityType": "SubtractiveOrganizationalUnit",
+                                    "_refs": [
+                                        {
+                                            "label": "stableTargetId",
+                                            "position": 0,
+                                            "value": "e5rfAc2p5zV39WUVZeAR1",
+                                        },
+                                    ],
+                                },
+                                {
+                                    "entityType": "PreventiveOrganizationalUnit",
+                                    "_refs": [
+                                        {
+                                            "label": "stableTargetId",
+                                            "position": 0,
+                                            "value": "e5rfAc2p5zV39WUVZeAR1",
+                                        },
+                                    ],
+                                },
+                            ],
+                            "entityType": "MergedOrganizationalUnit",
+                            "identifier": "e5rfAc2p5zV39WUVZeAR1",
+                        }
+                    ],
+                    "total": 1,
+                }
+            ],
+            {
+                "items": [
+                    {
+                        "email": ["bar@foo.bar"],
+                        "identifier": "e5rfAc2p5zV39WUVZeAR1",
+                        "name": [
+                            {"language": "de", "value": "Eine unit von einer Org."},
+                        ],
+                    }
+                ],
+                "total": 1,
+            },
+        ),
+    ],
+    ids=["no_rules", "one_rule_raises_error", "three_rules"],
+)
+def test_search_merged_items_in_graph_mocked(
+    mocked_graph_result: list[dict[str, Any]], expected: Any, mocked_graph: MockedGraph
+) -> None:
+    mocked_graph.return_value = mocked_graph_result
+
+    try:
+        merged_result = search_merged_items_in_graph(stable_target_id="bFQoRhcVH5DHUB")
+    except Exception as error:
+        assert str(expected) in str(error), error
+    else:
+        assert merged_result.model_dump(exclude_defaults=True) == expected
