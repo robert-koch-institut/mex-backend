5407664c92474f65af40ea23af76b5903756ac70
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2025-01-27 13:13:35 +0100

    add test case for filtering merged items by primary source at merged endpoint

--- tests/merged/test_main.py
+++ tests/merged/test_main.py
@@ -164,7 +164,7 @@ def test_search_merged_items_mocked(
             },
         ),
         (
-            "?identifier=bFQoRhcVH5DHUx",
+            "?identifier=bFQoRhcVH5DHUz",
             {
                 "items": [
                     {
@@ -210,6 +210,65 @@ def test_search_merged_items_mocked(
                 "total": 1,
             },
         ),
+        (
+            "?hadPrimarySource=bFQoRhcVH5DHUt",
+            {
+                "items": [
+                    {
+                        "$type": "MergedOrganization",
+                        "alternativeName": [],
+                        "geprisId": [],
+                        "gndId": [],
+                        "identifier": "bFQoRhcVH5DHUv",
+                        "isniId": [],
+                        "officialName": [
+                            {"language": "de", "value": "RKI"},
+                            {"language": "en", "value": "Robert Koch Institute"},
+                            {
+                                "language": "de",
+                                "value": "Robert Koch Institut ist the best",
+                            },
+                        ],
+                        "rorId": [],
+                        "shortName": [],
+                        "viafId": [],
+                        "wikidataId": [],
+                    },
+                    {
+                        "$type": "MergedOrganizationalUnit",
+                        "alternativeName": [],
+                        "email": [],
+                        "identifier": "bFQoRhcVH5DHUF",
+                        "name": [
+                            {"language": "en", "value": "Unit 1.6"},
+                            {"language": "en", "value": "Unit 1.7"},
+                        ],
+                        "parentUnit": "bFQoRhcVH5DHUx",
+                        "shortName": [],
+                        "unitOf": ["bFQoRhcVH5DHUv"],
+                        "website": [
+                            {
+                                "language": None,
+                                "title": "Unit Homepage",
+                                "url": "https://unit-1-7",
+                            }
+                        ],
+                    },
+                    {
+                        "$type": "MergedOrganizationalUnit",
+                        "alternativeName": [],
+                        "email": [],
+                        "identifier": "bFQoRhcVH5DHUx",
+                        "name": [{"language": "en", "value": "Unit 1"}],
+                        "parentUnit": None,
+                        "shortName": [],
+                        "unitOf": ["bFQoRhcVH5DHUv"],
+                        "website": [],
+                    },
+                ],
+                "total": 3,
+            },
+        ),
         ("?identifier=thisIdDoesNotExist", {"items": [], "total": 0}),
         ("?q=queryNotFound", {"items": [], "total": 0}),
     ],
@@ -220,6 +279,7 @@ def test_search_merged_items_mocked(
         "full text search",
         "identifier filter",
         "identifier filter with composite result",
+        "had primary source filter",
         "identifier not found",
         "full text not found",
     ],
