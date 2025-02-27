db32c0dff9a13f2881bdaf9e585407ed8002447e
rababerladuseladim <rababerladuseladim@users.noreply.github.com>
2025-02-27 09:53:59 +0100

    feature/mx-1706 add primary source filter (#259)
    ### Questions
    - swagger does not render `hadPrimarySource` input form for
    `/merged-item` and `/preview-item` endpoint. No idea why or how to fix.
    
    
    ![image](https://github.com/user-attachments/assets/a9e6b323-d961-4a3b-ba70-69ac804af1d0)
    
    - FastAPI discussions mention a workaround, but it did not work for me:
    https://github.com/fastapi/fastapi/discussions/10654
    
    ### Changes
    <!-- Changes in existing functionality -->
    - preview and merged endpoint filter by multiple primary sources (was:
    one)

--- mex/backend/preview/main.py
+++ mex/backend/preview/main.py
@@ -39,7 +39,7 @@ def preview_items(  # noqa: PLR0913
     q: Annotated[str, Query(max_length=100)] = "",
     identifier: Identifier | None = None,
     entityType: Annotated[Sequence[MergedType], Query(max_length=len(MergedType))] = [],
-    hadPrimarySource: Identifier | None = None,
+    hadPrimarySource: Annotated[Sequence[Identifier] | None, Query()] = None,
     skip: Annotated[int, Query(ge=0, le=10e10)] = 0,
     limit: Annotated[int, Query(ge=1, le=100)] = 10,
 ) -> PreviewItemSearch:
@@ -52,7 +52,7 @@ def preview_items(  # noqa: PLR0913
         q,
         identifier,
         [str(t.value) for t in entityType or MergedType],
-        hadPrimarySource,
+        [str(s) for s in hadPrimarySource] if hadPrimarySource else None,
         skip,
         limit,
         validate_cardinality=False,
