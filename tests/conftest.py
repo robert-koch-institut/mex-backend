5fc22e70509daf52da54b4e7d9b676be427a85a2
Janina Esins <esinsj@rki.de>
2025-04-16 16:11:41 +0200

    need smaller test data sets to find test assertion error

--- tests/conftest.py
+++ tests/conftest.py
@@ -328,7 +328,7 @@ def dummy_data(
 def artificial_extracted_items(
     locale: str = "de_DE",
     seed: int = 42,
-    count: int = 500,
+    count: int = 1,
     chattiness: int = 25,
     stem_types: Sequence[str] = EXTRACTED_MODEL_CLASSES_BY_NAME,
 ) -> list[AnyExtractedModel]:
