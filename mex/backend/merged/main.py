740393bedad9b1b7144252925a56d2d27753529d
Nicolas Drebenstedt <drebenstedtn@rki.de>
2025-06-16 16:28:19 +0200

    add new endpoint for merging items

--- mex/backend/merged/main.py
+++ mex/backend/merged/main.py
@@ -2,13 +2,20 @@ from collections.abc import Sequence
 from typing import Annotated
 
 from fastapi import APIRouter, Query
+from starlette import status
 
 from mex.backend.merged.helpers import search_merged_items_in_graph
+from mex.backend.merged.models import MergeRequest
 from mex.backend.types import MergedType, Validation
 from mex.common.models import AnyMergedModel, PaginatedItemsContainer
 from mex.common.types import Identifier
 
 router = APIRouter()
+
+
+@router.get("/merge", status_code=status.HTTP_204_NO_CONTENT, tags=["editor"])
+def merge_item(request: MergeRequest) -> None:
+    """Assign an extracted item to a new merged item."""
 
 
 @router.get("/merged-item", tags=["editor"])
---
+++ mex/backend/merged/models.py
@@ -0,0 +1,29 @@
+from typing import Annotated
+
+from pydantic import Field
+
+from mex.common.models import BaseModel
+from mex.common.types import AnyExtractedIdentifier, AnyMergedIdentifier
+
+
+class MergeRequest(BaseModel):
+    """Request body for merging two items."""
+
+    identifier: Annotated[
+        AnyExtractedIdentifier,
+        Field(
+            description=(
+                "The identifier of an extracted item "
+                "that is to be assigned to a new merged item."
+            )
+        ),
+    ]
+    stableTargetId: Annotated[
+        AnyMergedIdentifier,
+        Field(
+            description=(
+                "The identifier of a merged item "
+                "that the extracted item is to be assigned to."
+            )
+        ),
+    ]
