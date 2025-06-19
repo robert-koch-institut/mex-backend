c7003384662edd2170de24463330ec658d0bff03
Nicolas Drebenstedt <drebenstedtn@rki.de>
2025-06-19 16:22:57 +0200

    fix test

--- tests/graph/test_exceptions.py
+++ tests/graph/test_exceptions.py
@@ -5,7 +5,7 @@ from mex.backend.graph.exceptions import IngestionError
 
 def test_ingestion_error() -> None:
     details = ErrorDetails(type="foo", loc=(9, 9), msg="danger", input=None)
-    error = IngestionError("Something is wrong", (details,), retryable=True)
+    error = IngestionError("Something is wrong", errors=(details,), retryable=True)
     assert error.args[0] == "Something is wrong"
     assert error.is_retryable()
     assert error.errors() == [details]
