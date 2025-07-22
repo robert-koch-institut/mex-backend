e2bbdf7d4e10faaf31a512dd0c073914271f3333
RKI | Metadata Exchange <121876825+RKIMetadataExchange@users.noreply.github.com>
2025-07-22 15:52:31 +0200

    Update dependency mex-common to >=0.63,<0.64 (#359)
    ### Changes
    
    - update mex-common to 0.63
    
    ---------
    
    Co-authored-by: Nicolas Drebenstedt <drebenstedtn@rki.de>

---
+++ mex/backend/auxiliary/utils.py
@@ -0,0 +1,23 @@
+from mex.backend.extracted.helpers import search_extracted_items_in_graph
+from mex.common.identity import get_provider
+from mex.common.models import AnyExtractedModel
+from mex.common.types import MergedPrimarySourceIdentifier
+
+
+def fetch_extracted_item_by_source_identifiers(
+    had_primary_source: MergedPrimarySourceIdentifier,
+    identifier_in_primary_source: str,
+) -> AnyExtractedModel | None:
+    """Fetch an extracted item by source identifiers."""
+    provider = get_provider()
+    identities = provider.fetch(
+        had_primary_source=had_primary_source,
+        identifier_in_primary_source=identifier_in_primary_source,
+    )
+    if identities:
+        container = search_extracted_items_in_graph(
+            stable_target_id=identities[0].stableTargetId
+        )
+        if container.items:
+            return container.items[0]
+    return None
