e11a56a79699fa9f43adc8c1ffc34e54bd9ffa44
Zehra Ciftci <CiftciZ@rki.de>
2025-10-02 15:11:59 +0200

    new testing entrypoint

---
+++ mex/backend/testing/ldap.py
@@ -0,0 +1,87 @@
+from collections import deque
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Query
+
+from mex.backend.graph.connector import GraphConnector
+from mex.backend.testing.security import has_write_access_ldap
+from mex.common.models import (
+    MEX_PRIMARY_SOURCE_STABLE_TARGET_ID,
+    ExtractedContactPoint,
+    ExtractedPerson,
+    MergedPerson,
+    PaginatedItemsContainer,
+)
+from mex.common.types import Email
+
+DEFAULT_LDAP_QUERY = "mex@rki.de"
+
+router = APIRouter()
+
+
+@router.post("/merged-person-from-login")
+def get_merged_person_from_login(
+    username: Annotated[str, Depends(has_write_access_ldap)],
+) -> MergedPerson:
+    """Return the merged person from the ldap information and verify the login."""
+    person = ExtractedPerson(
+        hadPrimarySource=MEX_PRIMARY_SOURCE_STABLE_TARGET_ID,
+        identifierInPrimarySource=username,
+        fullName=[username],
+        email=[Email(f"{username}@rki.com")],
+        orcidId=["1234-5678-9012-3456"],
+    )
+    connector = GraphConnector.get()
+    deque(connector.ingest_items([person]))
+
+    result = connector.fetch_merged_items(
+        query_string=None,
+        identifier=None,
+        entity_type="MergedPerson",
+        reference_field="identifierInPrimarySource",
+        referenced_identifiers=[username],
+        skip=0,
+        limit=1,
+    )
+    return result["items"]  # type: ignore[no-any-return]
+
+
+@router.get("/ldap", tags=["auxiliary"])
+def search_persons_or_contact_points_in_ldap(
+    q: Annotated[str, Query(max_length=1000)] = DEFAULT_LDAP_QUERY,
+    limit: Annotated[int, Query(ge=1, le=100)] = 10,
+) -> PaginatedItemsContainer[ExtractedPerson | ExtractedContactPoint]:
+    """Search for person or contact points in LDAP.
+
+    Args:
+        q: The name of the person or contact point
+        limit: The maximum number of results to return
+
+    Returns:
+        Paginated list of ExtractedPersons and ExtractedContactPoints
+    """
+    items = [
+        ExtractedPerson(
+            hadPrimarySource=MEX_PRIMARY_SOURCE_STABLE_TARGET_ID,
+            identifierInPrimarySource="mex",
+            fullName=[f"{q}"],
+            email=[Email("mex@rki.com")],
+        ),
+        ExtractedPerson(
+            hadPrimarySource=MEX_PRIMARY_SOURCE_STABLE_TARGET_ID,
+            identifierInPrimarySource="mex2",
+            fullName=["mex2"],
+            email=[Email("mex2@rki.com")],
+        ),
+        ExtractedContactPoint(
+            hadPrimarySource=MEX_PRIMARY_SOURCE_STABLE_TARGET_ID,
+            identifierInPrimarySource="mex3",
+            email=[Email("mex3@rki.com")],
+        ),
+    ]
+    limited_items = items[:limit]
+
+    return PaginatedItemsContainer[ExtractedPerson | ExtractedContactPoint](
+        items=limited_items,
+        total=len(limited_items),
+    )
---
+++ mex/backend/testing/security.py
@@ -0,0 +1,61 @@
+from typing import Annotated
+
+from fastapi import Depends, Header, HTTPException
+from fastapi.security import APIKeyHeader, HTTPBasic, HTTPBasicCredentials
+from ldap3.utils.dn import escape_rdn
+from starlette import status
+
+X_API_KEY = APIKeyHeader(name="X-API-Key", auto_error=False)
+HTTP_BASIC_AUTH = HTTPBasic(auto_error=False)
+
+
+def _check_header_for_authorization_method(
+    api_key: Annotated[str | None, Depends(X_API_KEY)] = None,
+    credentials: Annotated[
+        HTTPBasicCredentials | None, Depends(HTTP_BASIC_AUTH)
+    ] = None,
+    user_agent: Annotated[str, Header(include_in_schema=False)] = "n/a",
+) -> None:
+    """Check authorization header for API key or credentials.
+
+    Raises:
+        HTTPException if both API key and credentials or none of them are in header.
+
+    Args:
+        api_key: the API key
+        credentials: username and password
+        user_agent: user-agent (in case of a web browser starts with "Mozilla/")
+    """
+    if not api_key and not credentials:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Missing authentication header X-API-Key or credentials.",
+            headers=(
+                {"WWW-Authenticate": "Basic"}
+                if user_agent.startswith("Mozilla/")
+                else None
+            ),
+        )
+    if api_key and credentials:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Authenticate with X-API-Key or credentials, not both.",
+            headers=(
+                {"WWW-Authenticate": "Basic"}
+                if user_agent.startswith("Mozilla/")
+                else None
+            ),
+        )
+
+
+def has_write_access_ldap(
+    credentials: Annotated[HTTPBasicCredentials, Depends(HTTP_BASIC_AUTH)],
+) -> str:
+    """Verify if provided credentials have LDAP write access.
+
+    Args:
+        credentials: username and password
+    """
+    _check_header_for_authorization_method(credentials=credentials)
+
+    return escape_rdn(credentials.username.split("@")[0])
