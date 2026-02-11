e85d6663545d89285097fc2a567b4b7b215adef7
Nicolas Drebenstedt <drebenstedtn@rki.de>
2026-02-11 15:04:38 +0100

    helper tests and changelog

--- mex/backend/match/main.py
+++ mex/backend/match/main.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from fastapi import APIRouter, Body, HTTPException, status
 
-from mex.backend.match.helper import match_item_in_graph
+from mex.backend.match.helpers import match_item_in_graph
 from mex.common.exceptions import MExError
 from mex.common.types import AnyExtractedIdentifier, AnyMergedIdentifier
 
