a18880a063d5649bd517a12c8a8bb1fa65c6f965
esinsj <72222648+esinsj@users.noreply.github.com>
2026-09-16 16:45:13 +0200

    improve variable naming
    Co-authored-by: Nicolas Drebenstedt <897972+cutoffthetop@users.noreply.github.com>
    Signed-off-by: esinsj <72222648+esinsj@users.noreply.github.com>

--- scripts/reset_preventive_rule_for_switched_off_merged_items.py
+++ scripts/reset_preventive_rule_for_switched_off_merged_items.py
@@ -69,8 +69,8 @@ def reset_preventive_rule_for_switched_off_merged_items(*, dry_run: bool) -> Non
             continue
 
         if not all(
-            targets in rule_set.workflow.forbiddenPublishingTarget
-            for targets in forbidden_targets
+            target in rule_set.workflow.forbiddenPublishingTarget
+            for target in forbidden_targets
         ):
             msg = (
                 f"all required fields are switched off but not all forbidden targets "
